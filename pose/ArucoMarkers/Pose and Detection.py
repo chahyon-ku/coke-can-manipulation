45c915c4cdd8afdb4529de98fc97faf64128104c
DESKTOP-O7Q75AR\afern <ferna437@morris.umn.edu>
2022-12-01 15:48:03 -0600

    ap

---
+++ pose/ArucoMarkers/Pose and Detection.py
@@ -0,0 +1,106 @@
+import numpy as np
+import time
+import cv2
+import matplotlib.pyplot as plt
+
+
+ARUCO_DICT = {
+    "DICT_4X4_50": cv2.aruco.DICT_4X4_50,
+    "DICT_4X4_100": cv2.aruco.DICT_4X4_100,
+    "DICT_4X4_250": cv2.aruco.DICT_4X4_250,
+    "DICT_4X4_1000": cv2.aruco.DICT_4X4_1000,
+    "DICT_5X5_50": cv2.aruco.DICT_5X5_50,
+    "DICT_5X5_100": cv2.aruco.DICT_5X5_100,
+    "DICT_5X5_250": cv2.aruco.DICT_5X5_250,
+    "DICT_5X5_1000": cv2.aruco.DICT_5X5_1000,
+    "DICT_6X6_50": cv2.aruco.DICT_6X6_50,
+    "DICT_6X6_100": cv2.aruco.DICT_6X6_100,
+    "DICT_6X6_250": cv2.aruco.DICT_6X6_250,
+    "DICT_6X6_1000": cv2.aruco.DICT_6X6_1000,
+    "DICT_7X7_50": cv2.aruco.DICT_7X7_50,
+    "DICT_7X7_100": cv2.aruco.DICT_7X7_100,
+    "DICT_7X7_250": cv2.aruco.DICT_7X7_250,
+    "DICT_7X7_1000": cv2.aruco.DICT_7X7_1000,
+    "DICT_ARUCO_ORIGINAL": cv2.aruco.DICT_ARUCO_ORIGINAL,
+    "DICT_APRILTAG_16h5": cv2.aruco.DICT_APRILTAG_16h5,
+    "DICT_APRILTAG_25h9": cv2.aruco.DICT_APRILTAG_25h9,
+    "DICT_APRILTAG_36h10": cv2.aruco.DICT_APRILTAG_36h10,
+    "DICT_APRILTAG_36h11": cv2.aruco.DICT_APRILTAG_36h11
+}
+
+
+def aruco_display(corners, ids, rejected, image):
+    if len(corners) > 0:
+
+        ids = ids.flatten()
+
+        for (markerCorner, markerID) in zip(corners, ids):
+            corners = markerCorner.reshape((4, 2))
+            (topLeft, topRight, bottomRight, bottomLeft) = corners
+
+            topRight = (int(topRight[0]), int(topRight[1]))
+            bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
+            bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
+            topLeft = (int(topLeft[0]), int(topLeft[1]))
+
+            cv2.line(image, topLeft, topRight, (0, 255, 0), 2)
+            cv2.line(image, topRight, bottomRight, (0, 255, 0), 2)
+            cv2.line(image, bottomRight, bottomLeft, (0, 255, 0), 2)
+            cv2.line(image, bottomLeft, topLeft, (0, 255, 0), 2)
+
+            cX = int((topLeft[0] + bottomRight[0]) / 2.0)
+            cY = int((topLeft[1] + bottomRight[1]) / 2.0)
+            cv2.circle(image, (cX, cY), 4, (0, 0, 255), -1)
+
+            cv2.putText(image, str(markerID), (topLeft[0], topLeft[1] - 10), cv2.FONT_HERSHEY_SIMPLEX,
+                        0.5, (0, 255, 0), 2)
+            print("[Inference] ArUco marker ID: {}".format(markerID))
+
+    return image
+
+
+def pose_estimation(frame, aruco_dict_type, matrix_coefficients, distortion_coefficients):
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    aruco_dict = cv2.aruco.Dictionary_get(aruco_dict_type)
+    parameters = cv2.aruco.DetectorParameters_create()
+
+    corners, ids, rejected = cv2.aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
+    if len(corners) > 0:
+        for i in range(0, len(ids)):
+            rvec, tvec, markerPoints = cv2.aruco.estimatePoseSingleMarkers(corners[i], .1, matrix_coefficients,
+                                                                           distortion_coefficients)
+
+            cv2.aruco.drawDetectedMarkers(frame, corners)
+
+    return rvec, tvec
+
+
+aruco_type = "DICT_4X4_100"
+
+arucoDict = cv2.aruco.Dictionary_get(ARUCO_DICT[aruco_type])
+
+arucoParams = cv2.aruco.DetectorParameters_create()
+
+print(arucoDict)
+
+print(arucoParams)
+
+image = cv2.imread("7_color.png")
+h, w, _ = image.shape
+
+width = 1000
+height = 562
+img = cv2.resize(image, (width, height), interpolation=cv2.INTER_CUBIC)
+intrinsic_matrix = np.array(((672.66, 0, 480), (0, 672.66, 270), (0, 0, 1)))
+corners, ids, rejected = cv2.aruco.detectMarkers(img, arucoDict, parameters=arucoParams)
+
+detected_markers = aruco_display(corners, ids, rejected, img)
+output1, output2 = pose_estimation(img, ARUCO_DICT[aruco_type], intrinsic_matrix, .01)
+detected_markers = cv2.drawFrameAxes(detected_markers, intrinsic_matrix, .01, output1, output2, .1)
+cv2.imshow('Marker ID', detected_markers)
+print("Translation=", output2)
+print("Rotation =", output1)
+key = cv2.waitKey(0)
+if key == 27:
+   cv2.destroyAllWindows()
+
---
+++ pose/ArucoMarkers/TemplateMatching.py
@@ -0,0 +1,119 @@
+import os
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.spatial.transform import Rotation as R
+
+
+def make_and_save_templates():
+    # importing template images
+    temp_image_m = np.array(cv2.imread('0_color.png', cv2.IMREAD_GRAYSCALE))
+    temp_image_l = np.array(cv2.imread('4_color.png', cv2.IMREAD_GRAYSCALE))
+    temp_image_r = np.array(cv2.imread('1_color.png', cv2.IMREAD_GRAYSCALE))
+
+
+    # Apply local threshholding to make the images more invariant
+    # Normalizing images
+    thresh_m = cv2.ximgproc.niBlackThreshold(temp_image_m, maxValue=255, type=cv2.THRESH_BINARY, blockSize=41, k=-0.2)
+    thresh_l = cv2.ximgproc.niBlackThreshold(temp_image_l, maxValue=255, type=cv2.THRESH_BINARY, blockSize=41, k=-0.2)
+    thresh_r = cv2.ximgproc.niBlackThreshold(temp_image_r, maxValue=255, type=cv2.THRESH_BINARY, blockSize=41, k=-0.2)
+
+    # Templates
+    template_m = thresh_m[112:198, 480:546]
+    template_l = thresh_l[242:364, 211:311]
+    template_r = thresh_r[145:249, 573:639]
+
+    os.makedirs('templates', exist_ok=True)
+    cv2.imwrite('templates/0.png', template_m)
+    cv2.imwrite('templates/1.png', template_l)
+    cv2.imwrite('templates/2.png', template_r)
+
+
+def main():
+    # intrinsic matrix
+    f = 672.6654
+
+    templates = [np.array(cv2.imread('templates/0.png', cv2.IMREAD_GRAYSCALE)),
+                 np.array(cv2.imread('templates/1.png', cv2.IMREAD_GRAYSCALE)),
+                 np.array(cv2.imread('templates/2.png', cv2.IMREAD_GRAYSCALE))]
+
+    # image we want to know the can location of
+    current_image = np.array(cv2.imread('7_color.png', cv2.IMREAD_GRAYSCALE))
+    current_depth_image = np.array(cv2.imread('7_depth.png', cv2.IMREAD_ANYDEPTH))
+    position_image = cv2.ximgproc.niBlackThreshold(current_image, maxValue=255, type=cv2.THRESH_BINARY, blockSize=41, k=-0.2)
+
+    # Similarity checking
+    similarities = [cv2.matchTemplate(position_image[300:540, 200:800], template, cv2.TM_CCORR_NORMED)
+                    for template in templates]
+
+    max_peak = None
+    peaks = []
+    for i_similarity, similarity in enumerate(similarities):
+        flat = np.reshape(similarity, -1)
+        value = np.max(flat)
+        loc = np.argmax(flat)
+        peaks.append((value, 300 + templates[i_similarity].shape[0] // 2 + loc // similarity.shape[1],
+                      200 + templates[i_similarity].shape[1] // 2 + loc % similarity.shape[1]))
+        if max_peak is None:
+            max_peak = peaks[-1]
+        elif max_peak[0] < peaks[-1][0]:
+            max_peak = peaks[-1]
+
+    z = current_depth_image[max_peak[1], max_peak[2]] * 0.00025
+    x = z * (max_peak[2] - 480) / f;
+    y = z * (max_peak[1] - 270) / f;
+
+    cam_pose = [0.37978148, 0.24435215, 1.57745443, 1.74390674, 0.03425322, -0.10934942];
+    camRaruco = R.from_euler('xyz', cam_pose[3:]).as_matrix()
+
+    camTaruco = np.zeros((4, 4))
+    camTaruco[:3, :3] = camRaruco
+    camTaruco[:3, 3] = cam_pose[:3]
+    camTaruco[3, 3] = 1
+
+    camTsoda = np.zeros((4, 4))
+    camTsoda[:3, :3] = camRaruco
+    camTsoda[:3, 3] = [x, y, z]
+    camTsoda[3, 3] = 1
+
+
+    arucoTsoda = np.linalg.inv(camTaruco) @ camTsoda
+
+    arucorvecsoda, _ = cv2.Rodrigues(arucoTsoda[:3, :3])
+    arucotvecsoda = arucoTsoda[:3, 3]
+
+    camrvecaruco, _ = cv2.Rodrigues(camTaruco[:3, :3])
+    camtvecaruco = camTaruco[:3, 3]
+    camrvecsoda, _ = cv2.Rodrigues(camTsoda[:3, :3])
+    camtvecsoda = camTsoda[:3, 3]
+
+
+    intrinsic_matrix = np.array(((672.66, 0, 480), (0, 672.66, 270), (0, 0, 1)))
+    current_image = cv2.drawFrameAxes(current_image, intrinsic_matrix*(960/1000), .01, camrvecaruco, camtvecaruco, .1)
+    current_image = cv2.drawFrameAxes(current_image, intrinsic_matrix, .01, camrvecsoda, camtvecsoda, .1)
+    plt.imshow(current_image)
+
+    pose = np.zeros((1, 6))
+    pose[0, :2] = arucotvecsoda[:2]
+    return pose
+
+    # for T in [camTaruco, camTsoda]:
+    #     origin = T @ np.array([0., 0., 0., 1.])
+    #     x_axis = T @ np.array([.1, 0., 0., 1.])
+    #     y_axis = T @ np.array([0., .1, 0., 1.])
+    #     z_axis = T @ np.array([0., 0., .1, 1.])
+    #     origin = [origin[0] / origin[2] * f + 480, origin[1] / origin[2] * f + 270]
+    #     x_axis = [x_axis[0] / x_axis[2] * f + 480, x_axis[1] / x_axis[2] * f + 270]
+    #     y_axis = [y_axis[0] / y_axis[2] * f + 480, y_axis[1] / y_axis[2] * f + 270]
+    #     z_axis = [z_axis[0] / z_axis[2] * f + 480, z_axis[1] / z_axis[2] * f + 270]
+    #
+    #     print(origin, x_axis, y_axis, z_axis)
+    #
+    #     plt.plot((origin[0], x_axis[0]), (origin[1], x_axis[1]), color='red', marker='o')
+    #     plt.plot((origin[0], y_axis[0]), (origin[1], y_axis[1]), color='green', marker='o')
+    #     plt.plot((origin[0], z_axis[0]), (origin[1], z_axis[1]), color='blue', marker='o')
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
